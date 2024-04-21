0d14c117bc676d346b34a8215a74263e29a4c621
monk3yd <jegajardog@gmail.com>
2024-04-21 00:07:09 -0300

    Add monk3yd env configuration

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+
+    devShell.${system} = devenv.lib.mkShell {
+      inherit inputs pkgs;
+
+      modules = [
+        ({ pkgs, config, ...}:
+        {
+          env.PROJECT_NAME = "gotth_poc";
+          env.AWS_PROFILE = "monk3yd-rlab";
+          env.AWS_ACCOUNT_ID = "963485456147";
+          env.AWS_REGION_NAME = "us-east-1";
+
+          languages.go.enable = true;
+
+          packages = [
+            pkgs.pulumi
+            pkgs.pulumiPackages.pulumi-language-go
+          ];
+        })
+      ];
+    };
+  };
+}
