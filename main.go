29a462705942a8b93392a31bef798f94799f4091
monk3yd <jegajardog@gmail.com>
2024-04-21 02:21:10 -0300

    Some fmt, some rename, logs when server init and manage server error

--- main.go
+++ main.go
@@ -2,35 +2,46 @@ package main
 
 import (
 	controllers "PocGotham/controllers"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	router := chi.NewRouter()
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		controllers.HandlerBase(w, r)
 	})
+
 	// Configurar el manejador para renderizar el HTML
-	r.Post("/partial-username-input-text", func(w http.ResponseWriter, r *http.Request) {
+	router.Post("/partial-username-input-text", func(w http.ResponseWriter, r *http.Request) {
 		controllers.MainControllerUsernameInputText(w, r)
-
 	})
-	r.Post("/partial-password-input-text", func(w http.ResponseWriter, r *http.Request) {
+
+	router.Post("/partial-password-input-text", func(w http.ResponseWriter, r *http.Request) {
 		controllers.MainControllerPasswordInputText(w, r)
-
 	})
-	r.Post("/partial-remember-checkbox", func(w http.ResponseWriter, r *http.Request) {
+
+	router.Post("/partial-remember-checkbox", func(w http.ResponseWriter, r *http.Request) {
 		controllers.HandlerCheckboxRemember(w, r)
-
 	})
-	r.Post("/submit-signup", func(w http.ResponseWriter, r *http.Request) {
+
+	router.Post("/submit-signup", func(w http.ResponseWriter, r *http.Request) {
 		controllers.FromHookUsernameInputText(w, r)
 		controllers.HandlerSignUpButton(w, r)
 	})
-	r.Get("/submit-signup", func(w http.ResponseWriter, r *http.Request) {
+
+	router.Get("/submit-signup", func(w http.ResponseWriter, r *http.Request) {
 		controllers.HandlerSignUpButton(w, r)
 	})
-	http.ListenAndServe(":8080", r)
+
+	log.Printf("Server starting on port 8080")
+
+	// Start the server with error handling
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
