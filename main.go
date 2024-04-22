67a869e3d2de9bdddf87a933e92f2983d37325ae
patriciojlg <patricio.jlg@gmail.com>
2024-04-21 23:36:35 -0400

    Added settings package to manage global constants

--- main.go
+++ main.go
@@ -2,39 +2,36 @@ package main
 
 import (
 	controllers "PocGotham/controllers"
+	settings "PocGotham/settings"
 	"log"
 	"log/slog"
 	"net/http"
-	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-  	// Router
+	// Router
 	router := chi.NewRouter()
 
 	// Middleware
 	router.Use(middleware.Logger)
 
-
 	// Routes
 	router.Get("/", controllers.HandlerBase)
-
-
+	router.Get("/partial-username-input-text", controllers.MainControllerUsernameInputText)
 	router.Post("/partial-username-input-text", controllers.MainControllerUsernameInputText)
+	router.Get("/partial-password-input-text", controllers.MainControllerPasswordInputText)
 	router.Post("/partial-password-input-text", controllers.MainControllerPasswordInputText)
 	router.Post("/partial-remember-checkbox", controllers.HandlerCheckboxRemember)
 
 	router.Get("/submit-signup", controllers.HandlerSignUpButton)
 	router.Post("/submit-signup", controllers.HandlerSignUpButton)
 
-	// Server
-	port := os.Getenv("PORT")
-	slog.Info("Server starting", "port", port)
+	slog.Info("Server starting", "port", settings.PORT)
 
-	err := http.ListenAndServe(port, router)
+	err := http.ListenAndServe(settings.SERVER, router)
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
---
+++ settings/settings.go
@@ -0,0 +1,7 @@
+package settings
+
+const (
+	PORT    = "8080"
+	SERVER  = "localhost:8080"
+	API_KEY = "your_api_key"
+)
